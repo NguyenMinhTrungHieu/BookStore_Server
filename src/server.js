26303b4b41b31db29edef435558c50aa22d5fe42
Trung Hiếu <nguyenminhtrunghieu694@gmail.com>
2024-10-14 17:36:35 +0700

    Update Server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,4 @@
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
-
 
 const app = express()
 
@@ -8,17 +6,7 @@ const hostname = 'localhost'
 const port = 8000
 
 app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.send('Hello from Server')
+  res.send('Hello from Server BookStore')
 })
 
 app.listen(port, hostname, () => {
